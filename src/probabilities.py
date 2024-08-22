9159ef032ab57f7e616f31fce7dfa0b8392d267f
JD93Lopez <juandavid.jdlea@gmail.com>
2024-08-21 19:07:14 -0500

    Separacion de funciones y pruebas unitarias.

--- src/probabilities.py
+++ src/probabilities.py
@@ -1,4 +1,4 @@
-from randomizer import indiceAleatorio, generarRandom
+from src.randomizer import indiceAleatorio, generarRandom
 import random
 
 #Arreglo de estadisticas de golpe por tipo y subtipo de heroe
@@ -29,7 +29,7 @@ hitStatsPerType = [
         'tipo-heroe': 'MAGO',
         'subtipo-heroe': 'FUEGO',
         'dano-minimo': 1,
-        'dano-maximo': 4,
+        'dano-maximo': 6,
         'dano-prob': 70,
         'dano-crit-prob': 5,
         'evadir-golpe-prob': 0,
@@ -40,7 +40,7 @@ hitStatsPerType = [
         'tipo-heroe': 'MAGO',
         'subtipo-heroe': 'HIELO',
         'dano-minimo': 1,
-        'dano-maximo': 4,
+        'dano-maximo': 6,
         'dano-prob': 70,
         'dano-crit-prob': 6,
         'evadir-golpe-prob': 0,
@@ -51,7 +51,7 @@ hitStatsPerType = [
         'tipo-heroe': 'PICARO',
         'subtipo-heroe': 'VENENO',
         'dano-minimo': 1,
-        'dano-maximo': 4,
+        'dano-maximo': 8,
         'dano-prob': 55,
         'dano-crit-prob': 10,
         'evadir-golpe-prob': 0,
@@ -62,7 +62,7 @@ hitStatsPerType = [
         'tipo-heroe': 'PICARO',
         'subtipo-heroe': 'MACHETE',
         'dano-minimo': 1,
-        'dano-maximo': 4,
+        'dano-maximo': 8,
         'dano-prob': 60,
         'dano-crit-prob': 8,
         'evadir-golpe-prob': 0,
@@ -71,67 +71,97 @@ hitStatsPerType = [
     },  
 ]
 
+#Calcular el daño aleatorio del héroe que golpea
+def calculateBaseDamage( hero, hitStats ):
+    return random.randint(hitStats['dano-minimo'], hitStats['dano-maximo']) + hero.get('dano', 0)
+
+#Iterar las estadisticas en busca del tipo y subtipo de heroe que golpea
+def searchHeroStats( hero ):
+    for hitStats in hitStatsPerType:
+        if( (hitStats['tipo-heroe'] == hero['tipo-heroe']) & 
+        (hitStats['subtipo-heroe'] == hero['subtipo-heroe']) ):
+            #Calcular el daño aleatorio del héroe que golpea
+            hero['damage'] = calculateBaseDamage( hero, hitStats )
+            return hero, hitStats
+    return null
+
+#rellenar críticos en matriz
+def fillMatrixCrit( percentageMatrix, hitStats, hero ):
+    #obtener filas llenadas hasta ahora
+    filledRows = len(percentageMatrix)
+    probability = hitStats['dano-crit-prob']
+    #calcular filas a rellenar
+    rows = 80000 * ((probability + hero.get('critico',0) )/100)
+    filledBefore = filledRows
+    #rellenar hasta que las filas alcancen el valor de las filas que llevaba + las nuevas
+    while( filledRows < (rows + filledBefore) ):
+        #insertar valor porcentaje de daño
+        percentageMatrix.append(random.randint(120,180))
+        filledRows+=1
+
+    return percentageMatrix
+
+#rellenar estadística en matriz
+def fillMatrixStat( percentageMatrix, probability, value ):
+    #obtener filas llenadas hasta ahora
+    filledRows = len(percentageMatrix)
+    if( filledRows >= 80000 ):
+        return percentageMatrix
+    #calcular filas a rellenar
+    rows = 80000 * (probability/100)
+    filledBefore = filledRows
+    #rellenar hasta que las filas alcancen el valor de las filas que llevaba + las nuevas
+    while( filledRows < (rows + filledBefore) ):
+        #insertar valor porcentaje de daño
+        percentageMatrix.append(value)
+        filledRows+=1
+    return percentageMatrix
+
+#rellena la matriz de ceros hasta que tenga 80000 filas
+def completeMatrix(percentageMatrix):
+    filledRows = len(percentageMatrix)
+    while(filledRows<80000):
+        percentageMatrix.append(0)
+        filledRows+=1
+    return percentageMatrix
+
+#rellenar matriz de probabilidades
+def fillProbsMatrix( hitStats, hero ):
+    percentageMatrix = []
+    #rellena las casillas de critico adicionando el boots de critico y hace de 120% a 180%
+    percentageMatrix = fillMatrixCrit( percentageMatrix, hitStats, hero )
+
+    #rellena las casillas de daño normal es decir 100%
+    percentageMatrix = fillMatrixStat( percentageMatrix, hitStats['dano-prob'], 100 )
+
+    #rellena las casillas de daño cuando evaden es decir 80%
+    percentageMatrix = fillMatrixStat( percentageMatrix, hitStats['evadir-golpe-prob'], 80 )
+
+    #rellena las casillas de daño cuando resisten es decir 60%
+    percentageMatrix = fillMatrixStat( percentageMatrix, hitStats['resistir-prob'], 60 )
+
+    #rellena las casillas de daño cuando escapan es decir 20%
+    percentageMatrix = fillMatrixStat( percentageMatrix, hitStats['escapar-prob'], 20 )
+
+    #rellena las casillas sobrantes con ceros (No causa daño)
+    percentageMatrix = completeMatrix( percentageMatrix )
+
+    return percentageMatrix
+
+#organiza todas las estadisticas del heroe para calcular el de daño aplicado
 def calculateDamage( hero ):
 
+    #comprobar que los campos necesarios existan
     if( hero.get('tipo-heroe', "error") == "error" ):
         return -1
     if( hero.get('subtipo-heroe', "error") == "error" ):
         return -1
 
     #Iterar las estadisticas en busca del tipo y subtipo de heroe que golpea
-    for hitStats in hitStatsPerType:
-        if( (hitStats['tipo-heroe'] == hero['tipo-heroe']) & 
-        (hitStats['subtipo-heroe'] == hero['subtipo-heroe']) ):
-            #Calcular el daño aleatorio del héroe que golpea
-            hero['damage'] = random.randint(hitStats['dano-minimo'], hitStats['dano-maximo']) + hero.get('dano', 0)
-    
-    percentageMatrix = []
-    cnt = 0
+    hero, hitStats = searchHeroStats( hero )
 
-    #rellena las casillas de daño normal es decir 100%
-    probability = hitStats['dano-prob']
-    rows = 80000 * (probability/100)
-    actual = cnt
-    while( cnt < (rows + actual) ):
-        percentageMatrix.append(100)
-        cnt+=1
-
-    #rellena las casillas de critico adicionando el boots de critico y hace de 120% a 180%
-    probability = hitStats['dano-crit-prob']
-    rows = 80000 * ((probability+ hero.get('critico',0) )/100)
-    actual = cnt
-    while( cnt < (rows + actual) ):
-        percentageMatrix.append(random.randint(120,180))
-        cnt+=1
-
-    #rellena las casillas de daño cuando evaden es decir 80%
-    probability = hitStats['evadir-golpe-prob']
-    rows = 80000 * (probability/100)
-    actual = cnt
-    while( cnt < (rows + actual) ):
-        percentageMatrix.append(80)
-        cnt+=1
-
-    #rellena las casillas de daño cuando resisten es decir 60%
-    probability = hitStats['resistir-prob']
-    rows = 80000 * (probability/100)
-    actual = cnt
-    while( cnt < (rows + actual) ):
-        percentageMatrix.append(60)
-        cnt+=1
-
-    #rellena las casillas de daño cuando escapan es decir 20%
-    probability = hitStats['escapar-prob']
-    rows = 80000 * (probability/100)
-    actual = cnt
-    while( cnt < (rows + actual) ):
-        percentageMatrix.append(20)
-        cnt+=1
-
-    #rellena las casillas sobrantes con ceros (No causa daño)
-    while(cnt<80000):
-        percentageMatrix.append(0)
-        cnt+=1
+    #rellenar matriz de probabilidades
+    percentageMatrix = fillProbsMatrix( hitStats, hero )
 
     #se selecciona un porcentaje en una posición al azar
     damagePercentage = percentageMatrix[generarRandom()]
@@ -141,10 +171,20 @@ def calculateDamage( hero ):
 
 #prueba
 # hero = {
-#     'critico': 1,
-#     'dano':1,
+#     'critico': 0,
+#     'dano':0,
 #     'tipo-heroe': 'GUERRERO',
 #     'subtipo-heroe': 'TANQUE',
 # }
 # print(calculateDamage( hero ))
-#{ "critico": 1, "dano":1, "tipo-heroe": "GUERRERO", "subtipo-heroe": "TANQUE" }
+# { "critico": 1, "dano":1, "tipo-heroe": "GUERRERO", "subtipo-heroe": "TANQUE" }
+
+#contador de campos repetidos en matriz
+# counter = {}
+# for e in percentageMatrix:
+#     if(counter.get(e,"no esta")=="no esta"):
+#         counter[e] = 1
+#     else:
+#         counter[e] += 1
+# print(counter)
+# print(len(percentageMatrix))
